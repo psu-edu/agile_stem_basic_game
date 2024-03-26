33e85cf0848c11d29581567d1e4102b0023f943e
Aidanjm13 <ajmathu13@outlook.com>
2024-03-26 12:22:29 -0400

    Basic RayCasting System Made, Needs Testing

---
+++ AGILE_STEM_GAME/Assets/RayCasting/RayCaster.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1162976657ae38b022cf7cbfbbf1b106685c316a6da30582236cd36f9e474542
+size 905
---
+++ AGILE_STEM_GAME/Assets/RayCasting/RayReciever.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd2db39ea13e6a8f74e0a5a3379fc4d12b67e020372d0cf65877b8cdb190a6be
+size 916
