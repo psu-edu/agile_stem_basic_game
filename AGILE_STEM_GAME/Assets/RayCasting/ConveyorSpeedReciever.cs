d8325b5e9ac28f26724abdcd3cbd0c4da2f5bc7a
Aidanjm13 <ajmathu13@outlook.com>
2024-04-09 13:08:48 -0400

    Generic Reciever

--- AGILE_STEM_GAME/Assets/RayCasting/ConveyorSpeedReciever.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b2b2048f439ba0288150836ea05cc312eaea356bb2d1c2777a522bcfde2b01c
-size 936
---
+++ AGILE_STEM_GAME/Assets/RayCasting/RayReciever.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72ddf38eb613d91e8ee9d4296a1fe39ae6658b5f64c1455e94300c5404501533
+size 934
