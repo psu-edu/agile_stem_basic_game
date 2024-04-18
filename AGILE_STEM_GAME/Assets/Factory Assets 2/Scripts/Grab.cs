78f035b1bbc748720677343fab7a988726785290
cjpflugh <162357513+cjpflugh@users.noreply.github.com>
2024-04-18 13:13:18 -0400

    robot can now grab and stack
    robot can now grab and stack packages from conveyer belt onto a pallet. However, I think some inconsistent scaling needs to be addressed. Currently, the water bottle assets are much too large, and making a relatively scaled water bottle package would be huge. I feel the bottles need to be scaled down at least by half.

--- AGILE_STEM_GAME/Assets/Factory Assets 2/Scripts/Grab.cs
+++ AGILE_STEM_GAME/Assets/Factory Assets 2/Scripts/Grab.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dba4561a2697e11bd1bea09a598fc425469a51251378e4f5b2e3a9fd2b10025f
-size 1840
+oid sha256:95dc0fb7c3549b073f4b6c93ee3b9725b8c89ca6b42ebea115d2546c44581f93
+size 2114
