060247582b2f7494868b4e9134bb3592f1f7b1a2
Whiiizz <hongw@lafayette.edu>
2024-05-19 09:59:56 -0400

    Added animation to windows
    Added animation to windows of the water filling machine asset. New prefab is in the Blender Asset folder.

---
+++ AGILE_STEM_GAME/Assets/_Scripts/Gear_Rotation.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63363871b60c7a7a1d7a478b28262e0297b34351548d00db25a47f2aaa734c87
+size 414
---
+++ AGILE_STEM_GAME/Assets/_Scripts/move_forward_bottle.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ba4c97b83ae0bd0681e97bc8a779051b214ebfc0d74cacf9b9a93d2b6c3ef75
+size 459
