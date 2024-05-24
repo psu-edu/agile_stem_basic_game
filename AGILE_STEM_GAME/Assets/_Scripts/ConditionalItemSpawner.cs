94841a81b2970ece89c27c730eb1446f0ada8e9a
cjpflugh <cjpflugh2@gmail.com>
2024-05-24 11:49:01 -0400

    updated my Manufacturing Scene
    updated my Manufacturing Scene

---
+++ AGILE_STEM_GAME/Assets/_Scripts/ConditionalItemSpawner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27c9a799a7f47de80fa212b095870960f9c94b367b6971dc65035d0473fdc1c1
+size 1198
---
+++ AGILE_STEM_GAME/Assets/_Scripts/ItemSwapper.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bec42d5f7dcba13452943f6e3be28d94b18ba01991615147e52f43e53b5f8b24
+size 997
