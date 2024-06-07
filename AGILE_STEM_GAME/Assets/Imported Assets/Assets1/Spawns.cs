0f7ee32e4e5d692f34e8c92626d81734b0cbcaab
Aidanjm13 <ajmathu13@outlook.com>
2024-06-07 13:53:44 -0400

    Input Prefab Created

--- AGILE_STEM_GAME/Assets/Imported Assets/Assets1/Spawns.cs
+++ AGILE_STEM_GAME/Assets/Imported Assets/Assets1/Spawns.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:718858ad7459cafca0e755949d6d09dfe65f6545389f1816eb7007b57360894b
-size 863
+oid sha256:7b8775ac14de22b395fd8eff1834f53abc330768052d30128b1b40c8d6cb8fd7
+size 981
---
+++ AGILE_STEM_GAME/Assets/_Scripts/TextInput.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb54bff872e6ca84c10c3b6a9c1b24393daad4043b279382405a88729940597f
+size 1974
