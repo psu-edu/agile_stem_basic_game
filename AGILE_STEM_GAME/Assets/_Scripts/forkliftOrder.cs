1c8589d6156c19f7b0795982b89e561cfcfa15b4
Aidanjm13 <ajmathu13@outlook.com>
2024-06-13 22:26:55 -0400

    Working on what causes crashes

--- AGILE_STEM_GAME/Assets/_Scripts/forkliftOrder.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/forkliftOrder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a91e54fe21c1cff3851f19901c6b4ab8f8bb22f31263fc0a8aa2d0b980a8b367
-size 7676
+oid sha256:a94f443bc812760ffab3e170af38b0baac12b4ef4a58abffca0470e3d389cb6f
+size 7706
