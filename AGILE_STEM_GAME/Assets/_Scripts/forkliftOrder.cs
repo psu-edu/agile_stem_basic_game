70d370f935e264db88ad852f86d72fdb2b160c22
Aidanjm13 <ajmathu13@outlook.com>
2024-06-13 20:31:09 -0400

    Forklift goes in, dumps, and goes, back, crashing is called by a accessing something that they don't have access to

--- AGILE_STEM_GAME/Assets/_Scripts/forkliftOrder.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/forkliftOrder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc3623072239160b68bab6152d1bcb548101603f10b7cb44d75bda3db4601dbf
-size 4418
+oid sha256:a91e54fe21c1cff3851f19901c6b4ab8f8bb22f31263fc0a8aa2d0b980a8b367
+size 7676
