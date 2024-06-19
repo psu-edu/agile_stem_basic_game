11f52266d2967a868af3526366f3a2d7648d8961
Aidanjm13 <ajmathu13@outlook.com>
2024-06-19 17:36:17 -0400

    Added text input to conveyor speed controllers

--- AGILE_STEM_GAME/Assets/_Scripts/Button Systems/SpeedController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Button Systems/SpeedController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7c853a9027c9d94c43b320eb94e8f3f502f6a83e4aaeea2f31c4cb08b8cc2b1
-size 2314
+oid sha256:4282cb631bf3a8a8221c88d93bffd3c852eb7f0e97bee3bc1dd139f0f8ede5b0
+size 2764
