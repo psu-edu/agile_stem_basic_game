e7841dda45fe22a188371aea4fb4f4736bbf9f18
Whiiizz <hongw@lafayette.edu>
2024-06-22 17:47:25 -0400

    Crash Fixes
    Fixed the crash where deleted object's collider is still being used by cloth. Removed double object swapper in bottle production machine and added capsule collider back to water bottles

--- AGILE_STEM_GAME/Assets/_Scripts/prefab_cloth_collision.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/prefab_cloth_collision.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c23b137e4f1227148ffdfbfb4bde439f90d01bde7fbd336c44331b2b90334e4
-size 2957
+oid sha256:69a0561470b7b2f5f279f16363c194d7e56fcdcac6ef247d56bf5c93a082a096
+size 3264
--- AGILE_STEM_GAME/Assets/_Scripts/unheated_plastic_spawner.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/unheated_plastic_spawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8776552c748f0b91aa864bae2a9c0fc7fe28667b78b57c4e4fb9f6f16754a2d
-size 3509
+oid sha256:33f7019890137089d3fde742d6514ccb82447bbccb79202493768d1614307f7a
+size 4068
