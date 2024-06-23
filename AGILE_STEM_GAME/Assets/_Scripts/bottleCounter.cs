4b622ea945daccd763a1f3a6e7fdd8693298140a
Aidanjm13 <ajmathu13@outlook.com>
2024-06-23 18:24:49 -0400

    signs rotate and disappear
    the signs now rotate and disappear, working on the bottle counters workign with packages of different sizes

---
+++ AGILE_STEM_GAME/Assets/_Scripts/bottleCounter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d93d8e6f564b5506655d9d1265a25da8c1d12899caed50333b50bb3d9d94f062
+size 2019
--- AGILE_STEM_GAME/Assets/_Scripts/rotatingSign.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/rotatingSign.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:438011253debfdbf32ad99fc7ad87224f56b38e58e4b07526d1c1a01eff7bf3b
-size 1629
+oid sha256:0faf070362e53f6bd2244cd611a301df61ddb866271c64ad93199ddb1df10435
+size 1894
