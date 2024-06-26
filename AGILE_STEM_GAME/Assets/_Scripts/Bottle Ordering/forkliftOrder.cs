708b099b1a64ab48e358c7531103a8226ef68047
Aidanjm13 <ajmathu13@outlook.com>
2024-06-26 18:27:15 -0400

    Can Make Custom Orders for One Site
    For now you can make custom orders for one site, it creates a queue of orders and then makes them, logic is in place for multiple lines, but wont be implemented until we have pathing or another method, will implement a display board for orders

---
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/forkliftOrder.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3481e8ba80cc2f7cdcfbf33a79796e3642c2f7fe24d6d7724dd9360a4c80e2a9
+size 14170
--- AGILE_STEM_GAME/Assets/_Scripts/forkliftOrder.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5349f70cd29c6e4c4ef36370aae6997bb051a89f47f18495cfa19d16fd069f63
-size 11787
