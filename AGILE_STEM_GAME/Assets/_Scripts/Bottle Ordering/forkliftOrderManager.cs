952e8f93b1d790f981e37956f4405140e1648689
Aidanjm13 <ajmathu13@outlook.com>
2024-07-01 21:53:13 -0400

    CoRoutines for leaving done, working on UI for ordering

--- AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/forkliftOrderManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/forkliftOrderManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e76be78a239c16fa75d5342b88ea2870165e58d2a810ec874f291662d72a27
-size 19039
+oid sha256:9f6524f202455a096a94283d8fb512bc72f3304baaf6c0bd2eb22d95ebc9de28
+size 22115
