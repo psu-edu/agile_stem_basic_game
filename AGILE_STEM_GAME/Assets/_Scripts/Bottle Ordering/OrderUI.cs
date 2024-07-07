b2b560515cedd09b990b12b055fab51600c58a46
Aidanjm13 <ajmathu13@outlook.com>
2024-07-06 22:20:43 -0400

    Added restrictions to ordering

--- AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ee835ee21eaccc8b9eb270cac7805ef935178b8ea93576bb7b53140677834aa
-size 2454
+oid sha256:6d85a9373c35f9ecc52264ea6552c641c1b0ada066b71649395af53e658cd4b5
+size 3277
