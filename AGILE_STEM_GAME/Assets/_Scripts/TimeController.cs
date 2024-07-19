f1d88911255017135028b1850296d379b73dc1e3
Aidanjm13 <ajmathu13@outlook.com>
2024-07-19 19:13:25 -0400

    Finalized current version

--- AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ab4012f40ba573d43c6767746f2846f494155c96bbaddedc04d39466860ab33
-size 1860
+oid sha256:ff2b68e0cee67eb8b356c95a724bac6e6f41a88f903f8ec8734e0516eb06f5da
+size 1868
