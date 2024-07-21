6fa185c881b6b3e2ebbedd8b7671813b478b41eb
Whiiizz <hongw@lafayette.edu>
2024-07-21 16:21:49 -0400

    Added a to-be tested model for performance
    Created a 48 bottle pack that incorporates all the meshes together to reduce batch down to ~24 instead of ~700 for prev. optimized. Needs more testing

---
+++ AGILE_STEM_GAME/Assets/MeshCombiner/Editor/MeshCombinerEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:680910b319b96591152a03563cc0aa978c4e96a1c5d484c7443a42afb54e6aa9
+size 5851
---
+++ AGILE_STEM_GAME/Assets/MeshCombiner/Scripts/MeshCombiner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37ff0d0689cf4e22a0704102a41831968105415ab478191788a018843ce4585e
+size 14646
