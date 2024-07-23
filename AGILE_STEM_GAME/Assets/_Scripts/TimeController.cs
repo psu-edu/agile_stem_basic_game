faf6bece079e85babe8c8aebfdf0cbee4aa6c1a7
Aidanjm13 <ajmathu13@outlook.com>
2024-07-23 01:36:56 -0400

    in game clock added and displayed to player

--- AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e4b2e7caed606699a79e6b18f3bbea62b059795974b14844d0c4826b92b5fc
-size 2698
+oid sha256:e82f5fe7adc2a766deced514cc010abae0459c4b9516a2a8120ae42a7d3c4cbc
+size 3306
