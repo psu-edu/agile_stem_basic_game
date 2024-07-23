010197869565882c9f11908cb7bb9f5168ce5a26
Aidanjm13 <ajmathu13@outlook.com>
2024-07-22 23:44:25 -0400

    Physics Speed Changes

--- AGILE_STEM_GAME/Assets/_Scripts/ForkliftScript.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/ForkliftScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:006f3af9cf4105201574a86cee1a0955b243be47f3e61b8c6fe04b943544b179
-size 7916
+oid sha256:d8e1a3b17b79089a445cf17ee9d302908f3000fcdfe06f4021a43d9373d296d1
+size 8483
--- AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/TimeController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff2b68e0cee67eb8b356c95a724bac6e6f41a88f903f8ec8734e0516eb06f5da
-size 1868
+oid sha256:5957907764ce501e19f6a941bdb2111865429501e788c672290a4358c8e58ed1
+size 2195
