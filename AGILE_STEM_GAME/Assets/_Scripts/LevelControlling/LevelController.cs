de0de121bb7f978e8bf22aa394747cb7439fd05a
Aidanjm13 <ajmathu13@outlook.com>
2024-08-06 18:14:14 -0400

    LevelController Updated
    LevelController split into level and stage/scene controller for easier reloading of scenes, UI updated as well

--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/LevelController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/LevelController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe83618327bd6e583a7aa41a612b1d228a9866472038469393be2ddd41c7cbea
-size 3305
+oid sha256:d672775e8cd34addce548d252025dd1b6da868534f9ab6e9b1ea719a20f92255
+size 2693
---
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/StageController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd1218f1a144284cbba0460cabd4764eea672f96bfe5814f93a1ccec414a7185
+size 2871
