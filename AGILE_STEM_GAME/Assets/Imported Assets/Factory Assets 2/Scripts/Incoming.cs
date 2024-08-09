15e35e8e19e0e4f53d80233ba1fa28408cc0f340
Aidanjm13 <ajmathu13@outlook.com>
2024-08-09 02:13:36 -0400

    Made Place Left Animation for robot arm

--- AGILE_STEM_GAME/Assets/Imported Assets/Factory Assets 2/Scripts/Incoming.cs
+++ AGILE_STEM_GAME/Assets/Imported Assets/Factory Assets 2/Scripts/Incoming.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:564a3a2a30126e6406b63c8f6f9a24ed3ecf43da7fb7b80fbae0ae294e7e0fc1
-size 1387
+oid sha256:4d3ce82a92b26184e93488bd65d83e0659b616529e96348a91987fae380263f4
+size 1392
