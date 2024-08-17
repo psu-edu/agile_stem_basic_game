441873a57252c3c8eacaf4dd7457ef51328425f0
Whiiizz <hongw@lafayette.edu>
2024-08-17 13:30:12 -0400

    Added Shelfing Scripts
    Shelf scripts that will determine what was stocked, what was taken, and if the shelfs are full.

---
+++ AGILE_STEM_GAME/Assets/shelfing_trigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cb9ee90b9da59993fc6024c0989e4874808fe5c553891ef8a3a7939ed39c5b5
+size 1799
---
+++ AGILE_STEM_GAME/Assets/warehouse_shelf.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1734614abc2db1a262afebf803cc47dde872bdc9e360f5a712a19b1bbeeb789
+size 1623
