ddf6de439e797b983e262de4abd90654c6f5d623
Whiiizz <hongw@lafayette.edu>
2024-09-08 20:27:09 -0400

    DropOff Pallet Complete
    Dropping off pallet mechanics is done, but the choosing of shelfs need to be added

--- AGILE_STEM_GAME/Assets/_Scripts/ForkliftScript.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/ForkliftScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b8e8b7f1701f5c2cd44f87fcd75218c5a5b8cba41bbe6c6d5e1e03e964f7ab
-size 8985
+oid sha256:5af439f87ae1621d4c8b79ae1aaf0d98652c03b39ec6da1e0a5d69ba97ed124d
+size 9096
--- AGILE_STEM_GAME/Assets/palletized_pickup.cs
+++ AGILE_STEM_GAME/Assets/palletized_pickup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85b5f8455efcd53e0e7ca49671a1693fdcfda43f4b07d21291a3ed4bd9b34862
-size 5193
+oid sha256:96622b2dd9a93f1f08d25a607d570b643ed38eb71de5d65aee0932dd812d418e
+size 7098
