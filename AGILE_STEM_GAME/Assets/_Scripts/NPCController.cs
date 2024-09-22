88bc76512794c60289d5b13f4e000bb0f8982abf
Aidanjm13 <ajmathu13@outlook.com>
2024-09-21 21:05:15 -0400

    More Animations added

--- AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e243a2176488aa5d237f231d344e2b9bba15f7ec4a2d1b0ed0fcaca0bb9e9b4
-size 3889
+oid sha256:16e8abc1ad6c83c987637e9a22429777f357673ef88afe5b76a3e206bee6325b
+size 4056
