7d2dcbe891cff2c894b23397402b91925654b43b
Aidanjm13 <ajmathu13@outlook.com>
2024-10-07 15:56:54 -0400

    Doors Open and Close with Forklifts, forklifts blink if cannot add

---
+++ AGILE_STEM_GAME/Assets/FactorySideTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dec9f229c6dff6ebfbdbb9ca09fc8ecf96653cb68456946f352f3ca2601d2363
+size 914
---
+++ AGILE_STEM_GAME/Assets/OrderSideTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc7fe9bda28fc586715e0bd6f3d0e4b738ff2b675e73a65b4130670abbca2620
+size 909
