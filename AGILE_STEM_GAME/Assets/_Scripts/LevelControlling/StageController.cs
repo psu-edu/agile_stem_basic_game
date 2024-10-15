440474de51a66c9c5311b489386414b2148ca745
Aidanjm13 <ajmathu13@outlook.com>
2024-10-14 20:55:26 -0400

    Camera Script Set Up to move camera based on waypoints

--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/StageController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/StageController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6386b879c31be29d64a1f2ca3cc3106f39d3f76da55502c9778624caf55574bc
-size 9492
+oid sha256:283cf0caec5c390f8fd959debe24a148eced44b7e75dd66c4cb61858270a41d5
+size 9490
