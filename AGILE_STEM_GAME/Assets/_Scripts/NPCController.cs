d671318e95afe2d6f22affb6533b3ab80c14d97b
Aidanjm13 <ajmathu13@outlook.com>
2024-11-05 13:51:30 -0500

    Made it break if stuck

--- AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60666b31c0ad731f74335d24a3b3473428f4ca351b77e99dccacdcda8ec3a2ac
-size 7683
+oid sha256:961c9776b925c02b2da46a44d2cd52d5ec936144163d783a0f75a92ceb663df1
+size 7889
