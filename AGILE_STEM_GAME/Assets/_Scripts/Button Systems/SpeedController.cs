1bd84be494800daf95a543618cd292187f26c1a2
Whiiizz <hongw@lafayette.edu>
2024-11-24 15:19:46 -0500

    Adding bottleneck events to Conveyor Line

--- AGILE_STEM_GAME/Assets/_Scripts/Button Systems/SpeedController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Button Systems/SpeedController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e99939c2bf4daaeefcc9c8f680a1cc30401130fe7595c0bf00a51ea933bc5047
-size 6122
+oid sha256:e78bf25bdd0922c6bac9278c5b6c06c3b5e392929e53a06971ffbc02b7aff1bc
+size 6038
---
+++ AGILE_STEM_GAME/Assets/manufactur_events.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3267dbc08568d09de9b9f2e1ad6ba6abed836596b5cb6fcc93067d740c257027
+size 2437
