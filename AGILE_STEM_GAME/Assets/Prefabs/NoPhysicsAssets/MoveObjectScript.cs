e785baaf93c3a942e4448b9cc52493c4729f4eff
Aidanjm13 <ajmathu13@outlook.com>
2024-11-24 16:54:59 -0500

    Working towards removing gravity

---
+++ AGILE_STEM_GAME/Assets/Prefabs/NoPhysicsAssets/MoveObjectScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03a14e126d5f406d4d4a6733457c8aead119cd2f1885f2332e198beb0942c078
+size 526
---
+++ AGILE_STEM_GAME/Assets/Prefabs/NoPhysicsAssets/NoPhysicsConveyorScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7901ba536d2814acba8d0069573f562978ff74b835fc41dd30f3262ba035055
+size 824
