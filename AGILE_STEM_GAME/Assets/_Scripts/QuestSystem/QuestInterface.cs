974fc003432ac7e45addf46279e73dc0576c3995
Whiiizz <hongw@lafayette.edu>
2025-02-09 14:55:48 -0500

    UI Restrictions
    Hotfix for restricting multiple UI being allowed to open at the same time. Now user must exit the first UI before opening a different one

--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6d18e90e5f5248c16ce4161e65b885e1b338857facf0197305d060a2ac20fae
-size 2134
+oid sha256:9c81ec8720425ccbb995f70191b5aa7c46d8ab8e6021d9a5d8876fab3b20d195
+size 2190
--- AGILE_STEM_GAME/Assets/tutorial_information.cs
+++ AGILE_STEM_GAME/Assets/tutorial_information.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2643aad9cc815358b1c86adb7343f42033ceed0b6ac520e3c152f8f90fb3699b
-size 1393
+oid sha256:d59f5cd0f076d2dc01986733cfda34f0a8ac0bb5e77d63acb8c2dd265110a88b
+size 1449
