44afb838f8a3ac74b44ddc361836a8fa0978e8a2
Aidanjm13 <ajmathu13@outlook.com>
2025-03-06 22:15:08 -0500

    Script Made For Machines and Getting Collider Sizes

---
+++ AGILE_STEM_GAME/Assets/_Scripts/ColliderSize.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e18d610cc122d78cbfbc2db1aa4c56e8d4f00055986bd2daf2819df9add9099a
+size 1657
---
+++ AGILE_STEM_GAME/Assets/_Scripts/MachineManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe1a09e36c22155832d8ac8ac0c7c92ad80da2b5676d481235c4edc9bb518c9c
+size 1969
