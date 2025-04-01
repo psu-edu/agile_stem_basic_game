37c4d63e63a8dca9029e0641889ed01585e04511
Aidanjm13 <ajmathu13@outlook.com>
2025-04-01 19:31:30 -0400

    Updates to Machine Speed Controllers

---
+++ AGILE_STEM_GAME/Assets/_Scripts/Button Systems/HeatingMachineSpeedController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:30a857f52215ecaec08aee0907e2a24116a722fe13d82da02ca502fa9a4919d5
+size 7545
---
+++ AGILE_STEM_GAME/Assets/_Scripts/Button Systems/PackagingMachineSpeedController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbe61ad64ca61b7a8b7bcd5ebffa8a93b360c69775670a295b164decab5595d2
+size 7549
