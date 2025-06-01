e40d94d8758d9a07fdddb02d016d9f5d4fd63c7e
Whiiizz <hongw@lafayette.edu>
2025-05-31 21:55:34 -0400

    Added Extra Information in JSON
    Added timestamp and username in the json file when users complete the questionnaire.

--- AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97775d75320c840dcd72edc7f24fc17c571a03ff79731ba590274203c0856299
-size 4445
+oid sha256:0b1a895a935fdc092a6a46eb9cbdc504ada01bd8e100f2f67dd96d4b2338b567
+size 4667
