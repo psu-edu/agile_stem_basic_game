b808e8350dcbbb787f30c3fd7ccdab650c4a069e
Aidanjm13 <ajmathu13@outlook.com>
2025-06-04 20:53:08 -0400

    Trying to make transitioning between scenes work

---
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/EscapeMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65daf1a2e12910ef450ce4265df9028502509f2b6a10c638a96741dab3d56a45
+size 2212
--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/LevelController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/LevelController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d3944e25c506e9b16487227e64824f7cb6102abdef9ba0bfff421e59a4c7cf0
-size 3932
+oid sha256:2b3a04ae3025f5676eeb91b41c04c9eaa7986ed8ce2f6305bfd49eeb047be514
+size 4336
