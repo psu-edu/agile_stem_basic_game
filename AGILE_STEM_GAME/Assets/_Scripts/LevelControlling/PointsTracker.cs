a30a4c48991383af1e5d508459a92ae26a993168
Aidanjm13 <ajmathu13@outlook.com>
2025-06-09 15:11:08 -0400

    Adding Code for tracking leaderboard

--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/PointsTracker.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/PointsTracker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fbfd7f4f4f9a43b3239e08cca09c2533976da001df279197c37c79ebf8ec614
-size 3788
+oid sha256:ce4efc2453d1a8a8cf9619cb452eef9be08ca1ee80dc2575d76d143f37f52ac0
+size 6069
