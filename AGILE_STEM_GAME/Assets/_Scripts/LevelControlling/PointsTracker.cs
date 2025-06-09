99239292a85a74a08a3ff8177bfa8383582acddf
Aidanjm13 <ajmathu13@outlook.com>
2025-06-09 14:53:32 -0400

    Shows Scores on Level Page

--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/PointsTracker.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/PointsTracker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87d7fd6459c386ececd400515efce84e15e92130775e695b8a822b71bd156536
-size 3327
+oid sha256:0fbfd7f4f4f9a43b3239e08cca09c2533976da001df279197c37c79ebf8ec614
+size 3788
