496519db8cd7f1b9b6765e2cee0903035f205ba8
Whiiizz <hongw@lafayette.edu>
2025-06-08 21:11:31 -0400

    Adding csv file for badges

---
+++ AGILE_STEM_GAME/Assets/BadgeSystem/BadgeSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:73dfdab6f86abf0b1e0255b22c0cdf3edcb2123e5c1926f27563d80a7baebbb0
+size 484
--- AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b1a895a935fdc092a6a46eb9cbdc504ada01bd8e100f2f67dd96d4b2338b567
-size 4667
+oid sha256:703f4e11779bb35b93851e2e8c2b9ac12df1bd3a35d974c18c97933d71388069
+size 4731
