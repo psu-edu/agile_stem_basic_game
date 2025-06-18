0f643e2e651d6beb2b40763e2f35d849394479df
Aidanjm13 <ajmathu13@outlook.com>
2025-06-18 18:11:03 -0400

    When order finalized, order time is adjusted based on the time controllers current time

--- AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac3021649831bc1de80c872613371d1179c6e98b4cd2c8a8c12d46e8541a2d20
-size 12467
+oid sha256:9fcae4a2e2dc4370877afaef03cb42e096bf07c95acd9a9c86c139efc81e681a
+size 13022
