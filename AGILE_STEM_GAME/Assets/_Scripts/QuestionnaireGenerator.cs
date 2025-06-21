4be8df166ff55e6295a5cc27294bd1faaafe1807
Whiiizz <hongw@lafayette.edu>
2025-06-21 16:33:43 -0400

    Starting HEXAD improvement
    HEXAD question answer prompt will be updated so there are less clicks required for user to answer

--- AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cac7a63dc2e3aed8cceefb38db690843a2cd1cacfa91e9ce016284867395f909
-size 6544
+oid sha256:37ad5494a92f3b45a0952922048978c7da9c5ab0495615042ef2fea25e357425
+size 6638
