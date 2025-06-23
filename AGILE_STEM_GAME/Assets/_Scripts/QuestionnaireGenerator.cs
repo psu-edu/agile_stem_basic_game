bf32147768c9b830c4c59b8b60a7c59315512c4c
Whiiizz <hongw@lafayette.edu>
2025-06-23 16:05:29 -0400

    Interactive Questioning
    User can now go to previous question to update their choice when making new login.

--- AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestionnaireGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37ad5494a92f3b45a0952922048978c7da9c5ab0495615042ef2fea25e357425
-size 6638
+oid sha256:aa0fc12b2e5738b166c3234c8e9c66162fc7412eed0eb70a820febeec0131f43
+size 8683
--- AGILE_STEM_GAME/Assets/_Scripts/Questionnaire_Answer.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Questionnaire_Answer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1109f2cd7657cb5dd84517d768f19f887799f4c17a1623826ea74724908c671
-size 3330
+oid sha256:0bbefffb260be34f51ade60d0b9c2897b5b4a7c30d7c363907a3b68f2ee24962
+size 3364
