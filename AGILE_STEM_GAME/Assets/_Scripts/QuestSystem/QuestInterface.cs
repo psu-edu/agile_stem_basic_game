cf786de393434ad9a0ff344c14d8aa4e9a960cce
Aidanjm13 <ajmathu13@outlook.com>
2025-06-23 15:32:48 -0400

    Points shown at end of level

--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c1f78b992bb15b75a2828570dff94488f6e8c08c3425340973002dd6c3af0f5
-size 2683
+oid sha256:95b5df788c2ad94c67a23e42633d4fcea12bb1a883e88ef25ea096c505882cec
+size 2938
