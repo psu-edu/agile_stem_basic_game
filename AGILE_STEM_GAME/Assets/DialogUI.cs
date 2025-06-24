5c52ab979cad333d802aa0505afe96356f03707d
Aidanjm13 <ajmathu13@outlook.com>
2025-06-24 16:51:40 -0400

    Dialog now properly displays player name and talking name

--- AGILE_STEM_GAME/Assets/DialogUI.cs
+++ AGILE_STEM_GAME/Assets/DialogUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3655cc34949719ad7c5b94aaf20c9c0e6cfc28b52392e8604bdf1049d6fc230f
-size 1360
+oid sha256:c715c7ab15efbb5ed60abd2295bb3200ca00dc84eaf89ae580bcea9b810bad99
+size 2878
