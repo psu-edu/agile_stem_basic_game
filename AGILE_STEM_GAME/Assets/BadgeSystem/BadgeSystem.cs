9fc1f9f6635c174249e0243911434af6f56330d1
Whiiizz <hongw@lafayette.edu>
2025-06-24 18:56:08 -0400

    UI updates
    Updated consistency for UI

--- AGILE_STEM_GAME/Assets/BadgeSystem/BadgeSystem.cs
+++ AGILE_STEM_GAME/Assets/BadgeSystem/BadgeSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab94feffd3bd705a935257a36778495c7b3ea379f45da54b218d3bcee6eded9f
-size 18945
+oid sha256:839c145c9fbfd2093b9a5677bc2ab6b5b14afae3a2aa70bd02ff8d30378ad999
+size 19078
--- AGILE_STEM_GAME/Assets/_Scripts/StartScreenLogin.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/StartScreenLogin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27f347f79eee49506d0f05e81d5dfd16bd0652ab3dfc59a68fa4d2c34cc0f184
-size 1487
+oid sha256:91e6f4e75312a16efadcda7a3eea2679d6e68e44165c426081b1f9b0aab23823
+size 1489
