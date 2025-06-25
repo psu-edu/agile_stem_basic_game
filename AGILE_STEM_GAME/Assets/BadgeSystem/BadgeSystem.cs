0fd7f4e5cb8b25ef81b24887b7b6b25920da27fc
Whiiizz <hongw@lafayette.edu>
2025-06-24 20:10:46 -0400

    Merging Changes with new MultiFact UI

--- AGILE_STEM_GAME/Assets/BadgeSystem/BadgeSystem.cs
+++ AGILE_STEM_GAME/Assets/BadgeSystem/BadgeSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:839c145c9fbfd2093b9a5677bc2ab6b5b14afae3a2aa70bd02ff8d30378ad999
-size 19078
+oid sha256:aa15f853cc3b193ef58611c62fd61deaf9b369860c28c4df96739598847a41e9
+size 19248
---
+++ AGILE_STEM_GAME/Assets/BadgeSystem/FindBadgeManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f71b744eb2b59f3596563adf431a3ee0cae6ad9e4cd68f19c0606fb876d0941
+size 1117
