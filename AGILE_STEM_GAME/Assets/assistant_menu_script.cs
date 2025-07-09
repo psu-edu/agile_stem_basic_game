54ee334d28660ad8aa44d4bcd138aa6f33474e91
mohammed0823 <mia5663@psu.edu>
2025-07-09 05:58:28 -0400

    Enabled prompt typing, need to disable other keys.

--- AGILE_STEM_GAME/Assets/assistant_menu_script.cs
+++ AGILE_STEM_GAME/Assets/assistant_menu_script.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a54dc2ec17452d185cfdbee2f9ebdf2573383c790d30be6b1c1605fdd37169ca
-size 1445
+oid sha256:587d38b1f2238fccdb792329f90135038f8a7c11fd3ff54a791caf4219e4c3a0
+size 1422
