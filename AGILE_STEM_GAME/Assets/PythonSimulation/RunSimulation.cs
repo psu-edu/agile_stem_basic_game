8ead76f94c304196e089463a89161d47925f78d0
mohammed0823 <mia5663@psu.edu>
2025-07-14 20:10:36 -0400

    Pass hard-coded game data to Python simulation via config.json

--- AGILE_STEM_GAME/Assets/PythonSimulation/RunSimulation.cs
+++ AGILE_STEM_GAME/Assets/PythonSimulation/RunSimulation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0c00c83c1ffaed5ab4fe7141221b3af7c48f5e87804f8028a003a2650d95fe3
-size 1098
+oid sha256:5cf008e59559ad67ee894df3fd02ea3ee126cb4bac49585fb38be89e8c629a3d
+size 2051
--- AGILE_STEM_GAME/Assets/PythonSimulation/manufacturing_simulation.py
+++ AGILE_STEM_GAME/Assets/PythonSimulation/manufacturing_simulation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82bf59db870e58758331e3f11b6bb93ca1d46fce8f00e76395b8db993ceb5a4d
-size 4815
+oid sha256:1cb7ff8facde9068e43fc5ce186295fad17c3a8ce19fe842c8bdc625cb8f4816
+size 5200
