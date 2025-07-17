73237a40002a77e8b4febda380de7d33b8116b4b
mohammed0823 <mia5663@psu.edu>
2025-07-17 11:06:01 -0400

    Fixed conflict and included changes in forklift order manager script

--- AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/forkliftOrderManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/forkliftOrderManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5ba2083f85ac428ec5101ed31b3e0d8e3d104e3d3970697247d42779ff9dbf5
-size 33103
+oid sha256:5c4fcfb6a8713d8680ab87e6b74165b6b5472dc97e327a3ab7acd1be0fe96a4b
+size 33137
