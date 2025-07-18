557a1d3080eab2b9bbcc222cf056fea611f7fc01
Aidanjm13 <ajmathu13@outlook.com>
2025-07-18 14:56:45 -0400

    Shows profit and such in final level completion screen

--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee5819a608c1adecd471077a02bf6897b5721513a0ba47c3e7df557771179356
-size 2943
+oid sha256:6de8a8304db2031959c4f63432559dc2cc2bedc799c645a4d099f1fccc73df26
+size 3001
