afba9dec455644c4716849f795627ef1d69199cd
Aidanjm13 <ajmathu13@outlook.com>
2025-08-05 01:48:11 -0400

    Created UI Manager and worked on notes

---
+++ AGILE_STEM_GAME/Assets/UIManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aaeebb77af1ee09f6ca3f59faabb1bee78f71e1ca47ea18e8cec97bacb17fece
+size 3859
--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6de8a8304db2031959c4f63432559dc2cc2bedc799c645a4d099f1fccc73df26
-size 3001
+oid sha256:7a7313c7a464de2513888ab47b46f7485b0d23f1ea3251ead55fcf177e34ba30
+size 3156
