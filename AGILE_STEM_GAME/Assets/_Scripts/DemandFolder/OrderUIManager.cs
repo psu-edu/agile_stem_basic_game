e3ec3144cd7f28d6be3871b691601ddbb947e21d
Aidanjm13 <ajmathu13@outlook.com>
2025-08-17 02:53:19 -0400

    changing storage of demand orders to class objects to allow easier access of information

--- AGILE_STEM_GAME/Assets/_Scripts/DemandFolder/OrderUIManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/DemandFolder/OrderUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c00f2dfa1a0b7ed4161a9164be4479076e75e27ba9564f436c53ef02746d58e
-size 4172
+oid sha256:3c0411c56179c9c1744a82b42c609f66ea3c88b480564ff035856461bbcaa12e
+size 4638
