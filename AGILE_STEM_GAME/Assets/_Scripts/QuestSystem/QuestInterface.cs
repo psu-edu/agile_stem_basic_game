491f91bd51754b126f6f46b71318ae1038002796
Aidanjm13 <ajmathu13@outlook.com>
2025-09-18 13:25:30 -0400

    Fixing issue with larger quest text overlapping

--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/QuestInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60e4cf7711d45573a7d60e570fe6b442d90667da0f3c2d273b2c14b702c7e5b6
-size 3171
+oid sha256:b69fbc8518ed8ba6b94b4a76f54c1adc0f833d54ac9f18beee773ffc04d6a9f0
+size 3616
