52414b6fd6d7b124b210f8b07f084c503f525989
Aidanjm13 <ajmathu13@outlook.com>
2025-10-08 21:37:40 -0400

    Reformatted Start Screen
    The start screen is visually restarted, will need to have functionality added once new data system is implemented

--- AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Bottle Ordering/OrderUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3de2a99fcb17e82c6a5b56787a9a075303384c2fea9ea8a9b5109604ee2501a9
-size 14951
+oid sha256:c9676cd0ef52832cd3ced7446bb7caf70bc782c1ab272151b855040ed36884c0
+size 14931
