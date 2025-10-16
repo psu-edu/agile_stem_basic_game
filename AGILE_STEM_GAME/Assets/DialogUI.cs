d990009bdd62f3d7b6a6b4cb4a947c58b33bfff3
Aidanjm13 <ajmathu13@outlook.com>
2025-10-15 20:45:32 -0400

    Bugs in start screen and tutorials fixed

--- AGILE_STEM_GAME/Assets/DialogUI.cs
+++ AGILE_STEM_GAME/Assets/DialogUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d5c5fedb031ef28c6b19221b2c6eb7986c9fc6c219ccc17dddeaab15b9464dd
-size 4422
+oid sha256:4be1779afbbf51a7151f8cb637c0e408a2d00696a640602bc30ee4c36dd1ea50
+size 4676
--- AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/Tutorial_Note_Created.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/QuestSystem/Tutorial_Note_Created.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fa2817723ca4dd88df42a187f16affbf3a27f15e08a6513e70f6c9c833b59df
-size 1079
+oid sha256:a94e2666704ab51fbe1226568d1cf03422150b420a9ca72b92504382c8028b31
+size 1150
