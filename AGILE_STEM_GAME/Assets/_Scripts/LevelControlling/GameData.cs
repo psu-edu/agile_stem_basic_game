d7c2515c48ca387bb53c2a1c437e35f9f0abc350
Aidanjm13 <ajmathu13@outlook.com>
2025-11-10 23:09:18 -0500

    Added Further Identifiers to Start Screen to prevent duplicates

--- AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/GameData.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/LevelControlling/GameData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628dc3f5e1628476f2c19dc2eb04bebfb63834d30e49dbd92947fe5be8acdf90
-size 1406
+oid sha256:07bdaf33709e4cd5c36f9a9d1f2eaa8f1e753fa7589de60c25b84ab6e60c1915
+size 1490
--- AGILE_STEM_GAME/Assets/_Scripts/StartScreenLogin.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/StartScreenLogin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddb450ab7e86018cf6cfc698bcff0961d25cc9dab97f4907bd78ae0365377f16
-size 1433
+oid sha256:fb6e86e8a4619859d87f62fda1885075422b7ab21f731e778107d4fa708a64ab
+size 1908
