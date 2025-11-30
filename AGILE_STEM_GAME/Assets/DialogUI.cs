d7194ed10f7efb3bd77cb2f80493283e2b403012
Aidanjm13 <ajmathu13@outlook.com>
2025-11-30 17:05:06 -0500

    Added More Directions to Messy Office

--- AGILE_STEM_GAME/Assets/DialogUI.cs
+++ AGILE_STEM_GAME/Assets/DialogUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e33590a8e7a6fbc84ed6fb789842f435afded0a2656ba083d50864e785aa6c5
-size 4725
+oid sha256:6b95909d4530bc47ddddb68b3ee62183ba985aecc00beb75d2c5db7d032e0971
+size 4756
