b6b4737519c99be3f8b2923bf10340e62f65454b
Aidanjm13 <ajmathu13@outlook.com>
2026-04-27 23:21:31 -0400

    Updated Buffer Machine to collect data and delete objects after interval to prevent lag

--- AGILE_STEM_GAME/Assets/BufferEntry.cs
+++ AGILE_STEM_GAME/Assets/BufferEntry.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d1225a01c3db6aad46a4a387afd5823948d1cda4a0522767b41c452a0bebdb6
-size 453
+oid sha256:bff23c3db11d393db7a519e424e9745912947abacdf59cd78e553b8753ad4a72
+size 619
--- AGILE_STEM_GAME/Assets/_Scripts/BufferScripts/BufferMachine.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/BufferScripts/BufferMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfac4137215c3ac3ffc98584308ebcb9ef5167ab6b98f501e67ac0cade102db8
-size 2146
+oid sha256:920f0f939e59512c5ce9a85ff4ec992536545eea9fd5d1ed4d1ecb7dd79eebeb
+size 3099
