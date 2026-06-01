5143502baafbf6590f03a778a8c12e6dcac75dc7
Ryan Leibowitz <leibowrj@lafayette.edu>
2026-06-01 17:23:16 -0400

    Added added moving between questions on notes individually and making individual notes for each section

--- AGILE_STEM_GAME/Assets/_Scripts/Notes/NotesManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Notes/NotesManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00564fa4e353361540290e379c0f03b3b123fe73053350290dcaa9ed9abf533d
-size 16088
+oid sha256:98a934e6f15695a1f77b851b7de8a513663675b475c61b3de9e1c5df96b56ad9
+size 22586
