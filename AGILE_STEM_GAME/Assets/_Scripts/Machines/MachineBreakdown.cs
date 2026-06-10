4715a3a4ee505dd753b63543ce493b70bc77cb66
Ryan Leibowitz <leibowrj@lafayette.edu>
2026-06-10 15:04:12 -0400

    added initial methods and testing color method

--- AGILE_STEM_GAME/Assets/_Scripts/Machines/MachineBreakdown.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/Machines/MachineBreakdown.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df55b0c8c57d5212fd02c5c097830a08c546b3613af07e41f36cbae37ac4c896
-size 1317
+oid sha256:60248d8aff5e141c96c593c3bc3da314913c037ab29ddce00870b42633963fae
+size 1615
