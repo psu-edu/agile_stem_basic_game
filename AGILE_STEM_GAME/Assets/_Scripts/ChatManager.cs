6e58c847eb4488683f9524083240076fe3d2d60e
Remyc0des <79231875+Valentinocodes@users.noreply.github.com>
2026-06-30 20:05:17 -0400

    fixed issue sending game state data to API, now chatbot could read game data.

--- AGILE_STEM_GAME/Assets/_Scripts/ChatManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/ChatManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eadd2b8ffbee5d683d3139a7e163cf0bb64ebaa1d3e23ac1d2a231fc636906a
-size 4978
+oid sha256:21931d929d7094bf6658586299b7f925ed9d80235feed9906c315fbf3fa877c2
+size 5082
