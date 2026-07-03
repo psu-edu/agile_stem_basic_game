1843305200d0bac1d0d72f974be657b00d10b4b2
Ryan Leibowitz <leibowrj@lafayette.edu>
2026-07-03 15:44:17 -0400

    Added in light object created script for Indicating percentage chance of breaking down created script for machine breaking down based on the speed of the machine

--- AGILE_STEM_GAME/Assets/_Scripts/Machines/MachineBreakdown.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f4501c5e1269c1d2c8e30b2ebac548bd5b8c99c768d79302612eb4b955a6ee2a
-size 1908
---
+++ AGILE_STEM_GAME/Assets/_Scripts/Machines/MachineRiskIndicatorDisplay.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:baf8b178ae61c08b70f038a73f8169b8f2842f4c65ed2b8f41e7e1f982c49cd2
+size 1168
