851c7d55c39a44c4fecd726e9fea316877389a52
William Butlerl <butlerwo@LOPEZBEC-RSRCH2.ad.lafayette.edu>
2026-07-29 16:04:12 -0400

    Added random NPC movement between lines and improved the NPC meet feature

--- AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/NPCController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d83d2cc7e3d8b0c2a7600ffac6255cadc6219c02df72ed4820551c44816901b
-size 10230
+oid sha256:817958a83fe4087c227606f34a9b6a27129111a48c87bea8bb6af321280b8123
+size 14426
---
+++ AGILE_STEM_GAME/Assets/_Scripts/PathGraph.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:95ba454b41033d7fc40116bfdacc6a4048fed93a79180654f4158c8be9037177
+size 7815
