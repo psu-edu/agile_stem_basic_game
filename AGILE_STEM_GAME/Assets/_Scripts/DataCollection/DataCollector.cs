edf986bad770424134d5c7286e88edd26e607934
Remyc0des <79231875+Valentinocodes@users.noreply.github.com>
2026-08-17 19:39:40 -0400

    added demand and current orders to datacollector

--- AGILE_STEM_GAME/Assets/_Scripts/DataCollection/DataCollector.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/DataCollection/DataCollector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13094da18a4a6e3f5f70d22c33b01573735a202990b3dedbd25405a68d43421d
-size 8999
+oid sha256:f47765b98ef3688295f8436fe3269298bfb6e343fa93af9426b30e3ad844af5a
+size 10717
