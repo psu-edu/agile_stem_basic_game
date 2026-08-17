5909f2284ad546217c446ab463ce20ebe8fea6d8
Remyc0des <79231875+Valentinocodes@users.noreply.github.com>
2026-08-17 18:15:09 -0400

    stable link for API, no need to start chat assitantup locally anymore

--- AGILE_STEM_GAME/Assets/_Scripts/ChatManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/ChatManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21931d929d7094bf6658586299b7f925ed9d80235feed9906c315fbf3fa877c2
-size 5082
+oid sha256:dd8303be7b773b8cbfff97f2a14f2600c14250f2ff525fa846c9ec5bf2f18f4f
+size 5096
