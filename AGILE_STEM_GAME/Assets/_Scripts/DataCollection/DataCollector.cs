021e98130c325c3ef8ffbe2876fe3708a7bd00b1
Remone Carter <rpc5906@psu.edu>
2026-09-01 16:11:18 -0400

    fixed syntax errors, added package getter functions

--- AGILE_STEM_GAME/Assets/_Scripts/DataCollection/DataCollector.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/DataCollection/DataCollector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f47765b98ef3688295f8436fe3269298bfb6e343fa93af9426b30e3ad844af5a
-size 10717
+oid sha256:a6960edadf9a142aace3d183dc2c4cc82ca01c1f6a7be955e92e64d069f98360
+size 10682
--- AGILE_STEM_GAME/Assets/_Scripts/DemandFolder/OrderUIManager.cs
+++ AGILE_STEM_GAME/Assets/_Scripts/DemandFolder/OrderUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d95887ae0242086d084c255e23c844652566e13c10bec098f67b143e60d092c
-size 15860
+oid sha256:52217acda0dd2da8d0dc6672817f53685a42da3f107bea1f1e48c6d7a31e94b9
+size 16184
